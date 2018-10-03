36482cdd0af23261e38f69d955eb8c78ffa69412
Adel Ferdjoukh <ferdjoukh@gmail.com>
2018-10-03 14:14:57 +0200

    voronoi.r : add a function for avoiding error with zero values in distance matrix

---
+++ tests/runEricTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#voronoiIt("testEric/levenshtein_matrix","testEric/test1.pdf","testEric/test2.tex")
+
+Rscript --vanilla "../voronoi.r" "levenshtein_matrix" "voronoiEric.pdf" "voronoiEric.tex"
--- voronoi.r
+++ voronoi.r
@@ -1,43 +1,87 @@
-	#Paquets requis
-	library("tripack")
-	require(tikzDevice)
-	library(MASS)
+#Required libs and packages
+library("tripack")
+require(tikzDevice)
+library(MASS)
+	
+# This function plots voronoi diagram for a given distance matrix (csv file)
+#
+# PARAMS
+# 	data: distance-matrix.csv
+# 	filepdf: output.pdf
+# 	filetex: output.tex
+#
+######
+voronoiIt <- function(data,filepdf,filetex){
 
-	#Three Parameters
-	# data: matrice de distance .csv
-	# filepdf: sortie .pdf
-	# filetex: sortie .tex
-	voronoiIt <- function(data,filepdf,filetex){
-
-	#Lire les donnees
+	#Read CSV file
 	matrix1 <-read.table(data, header=FALSE, sep=",")
 	m <- as.matrix(matrix1)
-	d <- m
-	r <- sammon(d)
+	
+	#Remove clone lines
+	#r <- unique(m)
 
-	# Calculer la tessellation de Voronoi
-	x <- r$points
+
+	r <- addEpsilonValues(m)
+	r <- sammon(r)
+
+	#Add labels to the computed points
+	x <- r$points	
 	dt <- data.frame(x)
 	names(dt) <- c('x', 'y')
-	dt$labels <- paste0('m', 1:nrow(x))
+
+	#dt$labels <- paste0('m', nonClones(m))
+	dt$labels <- paste0('m', 1:ncol(m))
 
 	#Dessiner le diagramme
 	#Pdf
 	pdf(filepdf)
-	plot(voronoi.mosaic(x[,1], x[,2]),axis=TRUE,
-		main="",xlab="",ylab="",title="")
+	plot(voronoi.mosaic(x[,1], x[,2]),axis=TRUE,main="",xlab="",ylab="",title="")
 	points(x,pch=1)
 	text(dt$x,dt$y,dt$labels, pos=3)
 	dev.off()
 
 	#Tikz
 	tikz(filetex)
-	plot(voronoi.mosaic(x[,1], x[,2]),axis=TRUE,
-		main="",xlab="",ylab="",title="")
+	plot(voronoi.mosaic(x[,1], x[,2]),axis=TRUE,main="",xlab="",ylab="",title="")
 	points(x,pch=1)
 	text(dt$x,dt$y,dt$labels, pos=3)
 	dev.off()
 	}
+
+
+# This function returns the line numbers of non Clone models
+# The result is used for labelling the voronoi diagram points
+nonClones <- function(matrix){
+
+	dup <- duplicated(matrix)
+	result <- vector()
+
+	for (i in 1:ncol(matrix)) {
+		if(!dup[i]){
+			result <- c(result,i)			
+		}
+	}
+	return(result)
+}
+
+# sammon cannot accept zero or negative for non-self values.
+# eg. matrix[1,2] = 0 is not possible
+# 
+# To avoid this case, we add an epsilon values to the matrix in the wrong cases
+#
+addEpsilonValues <- function(matrix){
+
+	for (i in 1:ncol(matrix)) {
+		for (j in 1:nrow(matrix)) {
+			if(matrix[i,j]<=0 && i!=j){
+				matrix[i,j]=0.0001
+			}
+		}
+	}
+
+	return(matrix)
+}
+
 
 ###############################
 # Get console args an do calls
